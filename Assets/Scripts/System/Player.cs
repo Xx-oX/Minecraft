190926e9192b821f6d76cb5b2136baa1b161cb24
ycwu4142 <ycwu4142@gmail.com>
2019-11-30 18:55:33 +0800

    Upload Player Controller

---
+++ Assets/Scripts/System/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float movingSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Movement();
+	}
+
+    // Use w/a/s/d to control 
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            // moving forward
+            transform.localPosition += movingSpeed * Time.deltaTime * transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            // moving back
+            transform.localPosition += movingSpeed * Time.deltaTime * -transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // moving left
+            transform.localPosition += movingSpeed * Time.deltaTime * -transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            // moving right
+            transform.localPosition += movingSpeed * Time.deltaTime * transform.right;
+        }
+    }
+}
