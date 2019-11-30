055ae374070ca8557894651e522b22a088f9ad49
ycwu4142 <ycwu4142@gmail.com>
2019-11-30 18:12:40 +0800

    fatal: pathspec 'all' did not match any files

---
+++ Assets/Scripts/System/Generator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+
+public class Generator : MonoBehaviour {
+
+    public Transform Brick;
+    public Transform Dirt;
+    public Transform Glass;
+    public Transform GoldOre;
+    public Transform Grass;
+    public Transform Lamp;
+    public Transform Log;
+    public Transform Stone;
+
+    public int width;
+    public int height;
+
+    // Use this for initialization
+    void Start () {
+        makePlatform(width,height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Generate a platform
+    void makePlatform(int width,int height)
+    {
+        for(int i = 0; i < width; ++i)
+        {
+            for(int j = 0; j < width; ++j)
+            {
+                Transform c = Instantiate(Grass);
+                c.parent = transform;
+                c.localPosition = new Vector3(-width/2+i,height,-width/2+j);
+            }
+        }
+    }
+}
